a2ea9f54fe0a05184a3d813d3483cdc26bd8fc72
yucesefa <48711705+yucesefa@users.noreply.github.com>
2023-03-18 13:14:38 +0300

    ExchangeCurrencyAPI

---
+++ app.js
@@ -0,0 +1,43 @@
+//elementleri secme
+
+const amountElement = document.querySelector("#amount");
+const firstSelect = document.querySelector("#firstCurrency");
+const secondSelect = document.querySelector("#secondCurrency");
+const currency = new Currency("USD", "TRY");
+const ui = new UI(firstSelect, secondSelect);
+
+eventListeners();
+
+function eventListeners() {
+
+
+    firstSelect.onchange = function () {
+
+        currency.changeFirstCurrency(firstSelect.options[firstSelect.selectedIndex].textContent)
+
+        ui.changeFirst();
+        // exchangeCurrency();
+    }
+    secondSelect.onchange = function () {
+
+        currency.changeSecondCurrency(secondSelect.options[secondSelect.selectedIndex].textContent)
+
+        ui.changeSecond();
+
+    }
+    amountElement.addEventListener("input", exchangeCurrency);
+
+
+
+}
+
+
+function exchangeCurrency() {
+
+    currency.changeAmount(amountElement.value);
+    currency.exchange()
+        .then(result => {
+            ui.displayResult(result);
+        })
+        .catch(err => console.log(err));
+}
